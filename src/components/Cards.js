44f4a1adadb4dc6c9528ce145e6f8ff00c2c5e5d
kadirumac <mr.k.umac@gmail.com>
2022-03-21 18:29:31 +0100

    new card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Option from "./Option";
+import './cards.scss';
 
 const Cards = () => {
   const [visible, setVisible] = useState(false);
@@ -10,17 +11,28 @@ const Cards = () => {
 
   return (
     <>
-      <div className="card">
-        <ul className="list-group ">
+      <div className="card bg-white">
+        <div className="avatar-div d-flex justify-content-between align-items-end bg-white p-2">
+          <img className="avatar" src="../assets/icons/avatar1.png" />
+          <div className="avatar-div--text mb-1">My name</div>
+          <img className="dots mb-3" src="../assets/icons/dots.png" />
+        </div>
+        <div className="info-div d-flex flex-column justify-content-around align-items-center">
+          <div className="w-50 d-flex justify-content-around">Self: <span> 300</span></div>
+          <div className="w-50 d-flex justify-content-around">Total: <span> 300</span></div>
+        </div>
+
+
+        {/* <ul className="list-group ">
           <li className="list-group-item">Name: Ahmet</li>
           <button className="icon" onClick={handleClick}>
             <i className="bi bi-three-dots-vertical"></i>{" "}
           </button>
           <li className="list-group-item">Value: 500</li>
           <li className="list-group-item">Total: 1000</li>
-        </ul>
+        </ul> */}
       </div>
-      {visible && <Option />}
+      <Option />
     </>
   );
 };
