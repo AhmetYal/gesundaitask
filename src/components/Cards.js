d5c8e4f2033df86123b7f2eefc060527a3972eca
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-03-22 09:32:26 +0300

    card düzenlemeleri

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Option from "./Option";
-import './cards.scss';
+import "./cards.scss";
 
 const Cards = () => {
   const [visible, setVisible] = useState(false);
@@ -13,26 +13,25 @@ const Cards = () => {
     <>
       <div className="card bg-white">
         <div className="avatar-div d-flex justify-content-between align-items-end bg-white p-2">
-          <img className="avatar" src="../assets/icons/avatar1.png" />
+          <img className="avatar" src="../assets/icons/avatar1.png" alt="" />
           <div className="avatar-div--text mb-1">My name</div>
-          <img className="dots mb-3" src="../assets/icons/dots.png" />
+          <img
+            className="dots mb-3"
+            src="../assets/icons/dots.png"
+            alt=""
+            onClick={handleClick}
+          />
         </div>
         <div className="info-div d-flex flex-column justify-content-around align-items-center">
-          <div className="w-50 d-flex justify-content-around">Self: <span> 300</span></div>
-          <div className="w-50 d-flex justify-content-around">Total: <span> 300</span></div>
+          <div className="w-50 d-flex justify-content-around">
+            Self: <span> 300</span>
+          </div>
+          <div className="w-50 d-flex justify-content-around">
+            Total: <span> 300</span>
+          </div>
         </div>
-
-
-        {/* <ul className="list-group ">
-          <li className="list-group-item">Name: Ahmet</li>
-          <button className="icon" onClick={handleClick}>
-            <i className="bi bi-three-dots-vertical"></i>{" "}
-          </button>
-          <li className="list-group-item">Value: 500</li>
-          <li className="list-group-item">Total: 1000</li>
-        </ul> */}
       </div>
-      <Option />
+      {visible && <Option />}
     </>
   );
 };
--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -9,16 +9,25 @@ const NewUser = () => {
   };
   return (
     <>
-      <div className="card">
-        <ul className="list-group ">
-          <input className="list-group-item" value="Ahmet" />
-          <button className="icon" onClick={handleClick}>
-            <i className="bi bi-three-dots-vertical"></i>
-          </button>
-          <input className="list-group-item" />
-          <input className="list-group-item" />
-          <button className="btn btn-success">Save</button>
-        </ul>
+      <div className="card bg-white">
+        <div className="avatar-div d-flex justify-content-between align-items-end bg-white p-2">
+          <img className="avatar" src="../assets/icons/avatar1.png" alt="" />
+          <div className="avatar-div--text mb-1">My name</div>
+          <img
+            className="dots mb-3"
+            src="../assets/icons/dots.png"
+            alt=""
+            onClick={handleClick}
+          />
+        </div>
+        <div className="info-div d-flex flex-column justify-content-around align-items-center">
+          <div className="w-50 d-flex justify-content-around">
+            Self: <span> 300</span>
+          </div>
+          <div className="w-50 d-flex justify-content-around">
+            Total: <span> 300</span>
+          </div>
+        </div>
       </div>
       {visible && <Option />}
     </>
