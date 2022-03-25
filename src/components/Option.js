12d8b2e9c1f6bbc391b2c3ec7e1e7f62953e97a4
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-03-25 09:43:53 +0300

    add button

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,11 +1,13 @@
-import React from "react";
-
-const Option = () => {
+const Option = ({ deletecard, adddownline }) => {
   return (
     <>
       <div className="card">
-        <button className="list-group-item">Add a downline</button>
-        <button className="list-group-item">Delete</button>
+        <button className="list-group-item" onClick={adddownline}>
+          Add a downline
+        </button>
+        <button className="list-group-item" onClick={deletecard}>
+          Delete
+        </button>
         <button className="list-group-item">Request income</button>
       </div>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,34 @@
-import React from "react";
+import { useState } from "react";
 import Cards from "../components/Cards";
 import NewUser from "../components/NewUser";
 
 const Home = () => {
+  const [card, setCard] = useState([<Cards />]);
+  //  const [newUser, setNewUser] = useState([<NewUser />]);
+
+  const addCard = () => {
+    setCard([...card, <Cards />]);
+  };
+
+  // const adddownline = () => {
+  //   setNewUser([...newUser, <NewUser />]);
+  // };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-2">
           <button className="btn btn-primary">SAVE</button>
-          <button className="btn btn-danger">ADD</button>
+          <button className="btn btn-danger" onClick={addCard}>
+            ADD
+          </button>
         </div>
         <div className="col-10">
-          <Cards />
+          {card}
           <div className="newuser">
+            {/* {newUser} */}
             <NewUser />
-            <NewUser />
+
             <NewUser />
           </div>
         </div>
